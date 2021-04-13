c29a66dbfe03f491b0ddc10155da3a31995367da
viter <chekhiv@gmail.com>
2021-04-13 19:11:06 +0300

    Lesson 126

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/event-list';
 
@@ -6,6 +8,13 @@ function HomePage(props) {
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
