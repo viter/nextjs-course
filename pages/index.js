55efb56ade687e0577d3a44c890b3dd8589080d6
viter <chekhiv@gmail.com>
2021-04-14 17:57:34 +0300

    Lesson 138

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,9 @@
-import Head from 'next/head';
-
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
-
-function HomePage(props) {
-  const featuredEvents = props.featuredEvents;
-
+function HomePage() {
   return (
     <div>
-      <Head>
-        <title>NextJS Events</title>
-        <meta
-          name='description'
-          content='Find a lot of great events that allow you to evolve...'
-        />
-      </Head>
-      <EventList items={featuredEvents} />
+      <h1>The Home Page</h1>
     </div>
   );
 }
 
 export default HomePage;
-
-export async function getStaticProps() {
-  return {
-    props: {
-      featuredEvents: await getFeaturedEvents(),
-    },
-    revalidate: 1800,
-  };
-}
