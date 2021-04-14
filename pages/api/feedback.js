679a5bd92ef82dd435bbfa6319032ce58a60a406
viter <chekhiv@gmail.com>
2021-04-15 00:43:03 +0300

    Lesson 140

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
 function handler(req, res) {
   if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedbackText = req.body.feedback;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text: feedbackText,
+    };
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: 'Success!', feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: 'This works!' });
   }
-  res.status(200).json({ message: 'This works!' });
 }
 
 export default handler;
