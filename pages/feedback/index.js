f4a114c4c1710d7e3c45ffefcb722b1892ca1cf3
viter <chekhiv@gmail.com>
2021-04-18 18:17:29 +0300

    Lesson 147

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,13 +1,27 @@
-import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+import { buildFeedbackPath, extractFeedback } from '../api/feedback/index';
+import { Fragment, useState } from 'react';
 
 function FeedbackPage(props) {
-  console.log(props);
+  const [feedbackData, setFeedbackData] = useState();
+  function loadFeedbackHandler(id) {
+    fetch(`/api/feedback/${id}`)
+      .then((response) => response.json())
+      .then((data) => setFeedbackData(data.feedback));
+  }
   return (
-    <ul>
-      {props.feedbackItems.map((item) => {
-        return <li key={item.id}>{item.text}</li>;
-      })}
-    </ul>
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
